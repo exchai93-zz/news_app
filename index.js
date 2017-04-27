21415cbf86e5719ae5a25873eaaf28fe932ffd16
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-27 17:29:13 +0100

    minor refactor

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ var guardian = 'http://content.guardianapis.com/books/2016/dec/07/top-10-cats-in
 
 newsController = new NewsController('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric');
 var json_obj = newsController.requestAndParseAPI();
-console.log(json_obj)
 
-var article = new Article(json_obj.main.temp + ' is the temperature!');
-var article2 = new Article(json_obj.name + ' is where we are!');
 var articleList = new ArticleList();
-articleList.storeArticle(article);
-articleList.storeArticle(article2);
+articleList.storeArticle(new Article(json_obj.main.temp + ' is the temperature!'));
+articleList.storeArticle(new Article(json_obj.name + ' is where we are!'));
+
 var articleListView = new ArticleListView(articleList);
 
 function renderHeadlines() {
