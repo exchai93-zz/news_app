afc8cc1ecaf11a8f56aed8fc4680046f80cc1fd3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-27 18:06:10 +0100

    a few possibly useful apis

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 var makers = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=';
-var guardian = 'http://content.guardianapis.com/books/2016/dec/07/top-10-cats-in-literature';
+var guardian = 'http://content.guardianapis.com';
+var cats = '/books/2016/dec/07/top-10-cats-in-literature';
+var uknews = '/uk-news';
+var freeweatherapi = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric';
 
-newsController = new NewsController('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric');
+newsController = new NewsController(freeweatherapi);
 var json_obj = newsController.requestAndParseAPI();
 
 var articleList = new ArticleList();
