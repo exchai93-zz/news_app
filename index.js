06678c487ff93407c40c5a3b985df953bec0cc57
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-27 17:23:14 +0100

    hand api functionality to news controller model

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-var article = new Article('Nippy News goes live!');
-var article2 = new Article('Cat rescued from tree');
-var articleList = new ArticleList;
+var makers = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=';
+var guardian = 'http://content.guardianapis.com/books/2016/dec/07/top-10-cats-in-literature';
+
+newsController = new NewsController('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric');
+var json_obj = newsController.requestAndParseAPI();
+console.log(json_obj)
+
+var article = new Article(json_obj.main.temp + ' is the temperature!');
+var article2 = new Article(json_obj.name + ' is where we are!');
+var articleList = new ArticleList();
 articleList.storeArticle(article);
 articleList.storeArticle(article2);
 var articleListView = new ArticleListView(articleList);
+
 function renderHeadlines() {
   var element = document.getElementById('headlines');
   element.innerHTML = articleListView.viewHeadlines();
--- src/news-controller.js
+++ src/news-controller.js
@@ -1,25 +1,20 @@
-var makers = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=';
-var guardian = 'http://content.guardianapis.com/books/2016/dec/07/top-10-cats-in-literature';
+(function(exports) {
 
-// function RequestAPI(url) {
+  function NewsController(url) {
+    this._url = url;
+  }
 
-// var request = new XMLHttpRequest();
-// request.open("GET", url, false);
-// request.send();
-// return(request.responseText);
-//
-// }
-//
-// var response = RequestAPI('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric');
-// var json_obj = JSON.parse(response);
-// console.log(json_obj);
-document.addEventListener('DOMContentLoaded', function () {
+  NewsController.prototype.requestAPI = function() {
+    var request = new XMLHttpRequest();
+    request.open("GET", this._url, false);
+    request.send();
+    return request.responseText;
+  };
 
-var request = new XMLHttpRequest();
-request.open("GET", makers + guardian, false);
-request.send();
-var response = request.responseText;
-var json_obj = JSON.parse(response);
-console.log(json_obj);
+  NewsController.prototype.requestAndParseAPI = function() {
+    json_obj = JSON.parse(this.requestAPI());
+    return json_obj;
+  };
 
-});
+  exports.NewsController = NewsController;
+})(this);
